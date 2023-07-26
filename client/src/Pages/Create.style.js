98823bf53988d57b7864d70eef492c1e9301947b
heowan <101536766+heowan@users.noreply.github.com>
2023-07-26 23:59:51 +0900

    feat: 게시물 만들기 페이지 뒤로가기 버튼 구현 및 스타일 수정

--- client/src/Pages/Create.style.js
+++ client/src/Pages/Create.style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Grayscale, Color } from '../color';
 
 const MakeStyle = {
   Container: styled.div`
@@ -8,16 +9,33 @@ const MakeStyle = {
     height: 100vh;
   `,
   Make: styled.div`
-    border: 1px solid #c6c6c6;
-    border-radius: 5px;
+    border: 1px solid ${Grayscale[20]};
+    border-radius: 12px;
     width: 700px;
     height: 400px;
     margin: 0 auto;
-    padding: 20px;
-    h4 {
-      font-weight: 500;
-      text-align: center;
+
+    > .title {
+      position: relative;
+      display: flex;
+      height: 60px;
+      border-bottom: 1px solid ${Grayscale[20]};
+      justify-content: center;
+      align-items: center;
+
+      > p {
+        font-size: 16px;
+        font-weight: 400;
+      }
+
+      > img {
+        position: absolute;
+        left: 20px;
+        width: 32px;
+        cursor: pointer;
+      }
     }
+
     img {
       width: 170px;
       border-radius: 5px;
@@ -28,13 +46,23 @@ const MakeStyle = {
     margin-top: 30px;
     display: flex;
     justify-content: space-between;
-    padding: 10px;
+    padding: 10px 30px;
     padding-top: 20px;
     a {
-      color: #000;
+      color: ${Grayscale[80]};
       font-size: 14px;
       text-decoration: none;
       text-align: center;
+      border-radius: 12px;
+
+      > p {
+        margin-bottom: 14px;
+      }
+
+      &:hover {
+        box-shadow: 2px 4px 12px 0px rgba(0, 0, 0, 0.15);
+        outline: 1px solid ${Color.Primary};
+      }
     }
   `,
 };
